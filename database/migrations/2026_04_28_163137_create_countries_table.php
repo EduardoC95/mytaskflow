deca8bee14e0b69dae37e374fcd71d1c0faf1071
EduardoC95 <eduardo.carvalho@trainee.inovcorp.com>
2026-04-28 17:39:07 +0100

    Add countries table and model

--- database/migrations/2026_04_28_163137_create_countries_table.php
+++ database/migrations/2026_04_28_163137_create_countries_table.php
@@ -6,20 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('countries', function (Blueprint $table) {
             $table->id();
+            $table->string('code', 2)->unique();
+            $table->string('name');
+            $table->boolean('is_active')->default(true);
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('countries');
