a9f1d72a3938a2630647c0aaf211be300d7b64b1
EduardoC95 <eduardo.carvalho@trainee.inovcorp.com>
2026-04-28 17:41:25 +0100

    Add VAT rates table and model

--- app/Models/VatRate.php
+++ app/Models/VatRate.php
@@ -3,8 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class VatRate extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'rate',
+        'is_active',
+    ];
+
+    protected $casts = [
+        'rate' => 'decimal:2',
+        'is_active' => 'boolean',
+    ];
 }
